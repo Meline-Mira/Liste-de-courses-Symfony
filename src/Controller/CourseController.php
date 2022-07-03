1a0b9ae72109058b4af9fb242ed780707e9e2cd3
Meline Mira <emeline.gineys@gmail.com>
2022-07-03 17:25:46 +0200

    Affichage de la liste de course et produits pris

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -2,15 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\AchatRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CourseController extends AbstractController
 {
+    private AchatRepository $achatRepository;
+
+    public function __construct(AchatRepository $achatRepository)
+    {
+        $this->achatRepository = $achatRepository;
+    }
+
     #[Route('/')]
     public function liste(): Response
     {
-        return $this->render('courses/liste.html.twig');
+        return $this->render('courses/liste.html.twig', [
+            'achats' => $this->achatRepository->findAll()
+        ]);
     }
 }
