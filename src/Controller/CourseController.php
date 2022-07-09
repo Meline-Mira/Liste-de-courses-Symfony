b8a7d901309b708012c029d25e0c4a9a9f9cd016
Meline Mira <emeline.gineys@gmail.com>
2022-07-09 19:17:02 +0200

    Création du formulaire et affichage

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Achat;
+use App\Form\Type\AchatType;
 use App\Repository\AchatRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,9 +21,20 @@ class CourseController extends AbstractController
 
     #[Route('/')]
     public function liste(): Response
-    {
+    {   
         return $this->render('courses/liste.html.twig', [
             'achats' => $this->achatRepository->findAll()
+        ]);
+    }
+
+    #[Route('/nouveau')]
+    public function nouveau(Request $request): Response
+    {   
+        $achat = new Achat();
+        $form = $this->createForm(AchatType::class, $achat);
+
+        return $this->renderForm('courses/nouveau.html.twig', [
+            'form' => $form,
         ]);
     }
 }
